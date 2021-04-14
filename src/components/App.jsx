ca9a3c3e282957cba7e881ccc4ea3421477ac97c
frankienglish <frankienglish0416@gmail.com>
2021-04-14 14:33:29 -0700

    Added functionality to add new list items with the add button

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,20 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
+  // create a useState for the input and a method to update it
+  const [inputText, setInputText] = useState("");
+  // create a useState to handle updating the list every time you add an item
+  const [items, setItems] = useState([]);
+
+  // every time the text is changed update it on the input line
+  function handleChange(event) {
+    const newValue = event.target.value;
+    setInputText(newValue);
+  }
+
+  // when an item is added with the add button, return all the previous items with the new one added
+  function addItem() {
+    setItems((prevItems) => {
+      return [...prevItems, inputText];
+    });
+    setInputText("");
+  }
+
   return (
     <div className="container">
       <div className="heading">
         <h1>To-Do List</h1>
       </div>
       <div className="form">
-        <input type="text" />
-        <button>
+        <input onChange={handleChange} type="text" value={inputText} />
+        <button onClick={addItem}>
           <span>Add</span>
         </button>
       </div>
       <div>
         <ul>
-          <li>A Item </li>
+          {items.map((todoItem) => (
+            <li>{todoItem}</li>
+          ))}
         </ul>
       </div>
     </div>
